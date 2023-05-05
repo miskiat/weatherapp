e163feb5d111a09b743f8b075c8aa1a7b82d83ce
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-05-05 21:51:41 +0100

    number

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ function App() {
       </header>
       <div className="sky">
         <img className="sun" src="/public/Image/sun-fill.svg" />
+        <h1 className="number">13</h1>
       </div>
     </div>
   );
