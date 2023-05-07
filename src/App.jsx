cfb1e4f08537fca38b0448c483b74e566d268c42
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-05-07 20:52:59 +0100

    location

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,12 @@ function App() {
         <img className="sun" src="/public/Image/sun-fill.svg" />
         <h1 className="number">13 o</h1>
         <div id="container"></div>
-        <div className="place">Ilorin,Kwara</div>
-        <div className="right">
-          <div className="time">7:00 PM</div>
-          <div>Sunset time, Monday</div>
+        <div className="location">
+          <div className="place">Ilorin,Kwara</div>
+          <div className="right">
+            <div className="time">7:00 PM</div>
+            <div>Sunset time, Monday</div>
+          </div>
         </div>
       </div>
     </div>
