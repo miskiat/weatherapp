8452443fb39f357658f8d2a2af78bcab76aaf50a
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-05-09 15:41:40 +0100

    atmosphere

---
+++ src/components/Atmosphere.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Atmosphere = () => {
+  return <div>Atmosphere</div>;
+};
+
+export default Atmosphere;
---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+export const atmosphere = [
+  {
+    icon: "",
+    title: "Humidity",
+    desc: "29%",
+  },
+  {
+    icon: "",
+    title: "Uv Index",
+    desc: "0 out of 10",
+  },
+  {
+    icon: "",
+    title: "Sunset",
+    desc: "7;50 pm",
+  },
+  {
+    icon: "",
+    title: "Sunrise",
+    desc: "6:35am",
+  },
+];
