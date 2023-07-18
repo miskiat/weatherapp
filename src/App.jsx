ffa8d72898f48f9a410651b157bc0776912a2ddc
Miskiat Badmus <badmusmiskiat@gmail.com>
2023-07-18 21:41:49 +0100

    API

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
+import axios from "axios";
+import { useState } from "react";
 import "./App.css";
 import Atmosphere from "./components/Atmosphere";
 import Skys from "./components/Sky";
 import { atmospheres, sky } from "./data/data";
 
 function App() {
+  const [city, setCity] = useState("");
+  const url = `https://pro.openweathermap.org/data/2.5/forecast/hourly?q=${city}&appid=cfbd3b28c4042d6276d991aa2d46973e`;
+  // console.log(city);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.get(url).then((res) => console.log(res));
+  };
   return (
     <div className="wrapper">
       <header className="header">
         <h1 className="name">Ilorin</h1>
         <div className="inputt">
-          <input placeholder="Enter city name...." type="name" />
+          <form onSubmit={handleSubmit}>
+            <input
+              placeholder="Enter city name...."
+              type="name"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+            />
+          </form>
+
           <img className="search-icon" src="/public/image/search.svg" />
         </div>
       </header>
